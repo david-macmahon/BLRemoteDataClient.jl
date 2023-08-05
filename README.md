6b9f540412b59eadcfe5cd62679f0a0787695eba
David MacMahon <davidm@astro.berkeley.edu>
2023-08-04 20:59:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# BLRemoteDataClient
+
+The *Breakthrough Listen Remote Data Client* provides convenient access to
+servers providing a RESTful web based interface to Breakthrough Listen data
+stored on the hosts on which the servers run.  A companion project,
+[BLRemoteDataServer.jl](
+https://github.com/david-macmahon/BLRemoteDataServer.jl) is a Julia package that
+provides such a service.
+
+## Installation
+
+In the Julia REPL, run:
+
+```julia
+import Pkg
+Pkg.add("https://github.com/david-macmahon/BLRemoteDataClient.jl")
+```
+
+## Client overview
+
+The client provides functions to access to the following RESTful API endpoints:
+
+| Endpoint  | Function  |
+|:----------|:----------|
+|/prefixes  | prefixes  |
+|/readdir   | readdir   |
+|/finddirs  | finddirs  |
+|/findfiles | findfiles |
+|/fbfiles   | fbfiles   |
+|/fbdata    | fbdata    |
+
+Each function takes any required parameters of the endpoint (usually a directory
+or file name) as the initial arguments, followed by optional `host` and `port`
+arguments, followed by optional keyword arguments corresponding to the optional
+endpoint parameters.
+
+The `host` and `port` arguments specify the host and port of the server to
+query.  If unspecified, these arguments default to the contents of `HOST` and
+`PORT` module variables.  These are `Ref`s whose values can be changed at
+runtime if desired.  `HOST` and `PORT` are initialized to the value of the
+`BL_REMOTE_DATA_SERVER_HOST` and `BL_REMOTE_DATA_SERVER_PORT` environment
+variables, resp.  If an environment variable is not defined, `HOST` is
+initialized to `"localhost"` and `PORT` is initialized to 8000.
+
+### Method signatures
+
+Here are the methods supported by the client:
+
+```
+prefixes([host, [port]])::Vector{String}
+readdir(dir, [host, [port]]; regex=".", join=true)::Vector{String}
+finddirs(dir, [host, [port]]; regex=".", join=true)::Vector{String}
+findfiles(dir, [host, [port]]; regex=".", join=true)::Vector{String}
+fbfiles(dir, [host, [port]]; regex="\\.(fil|h5)\$")::Vector{Dict{String,Any}}
+fbdata(fbname, [host, [port]]; chans=:, ifs=:, times=:, dropdims=())::Array{Float32}
+```
+
+See the doc string of each function for detailed usage.
+
+## Using the client with a single server
+
+To use the client with a single server you can pass the hostname and port of the
+server on each call to a client function OR you can store the server's hostname
+and port in the `HOST` and `PORT` variables.  Because they are `Ref`s you must
+index them with `[]` when getting or setting their value.  The following are
+equivalent:
+
+```julia
+# With explicit hostname
+julia> BLRemoteDataClient.prefixes("blc43")
+3-element Vector{String}:
+ "/datax"
+ "/datax2"
+ "/datax3"
+
+# With custom "default" hostname
+julia> BLRemoteDataClient.HOST[] = "blc43"
+"blc43"
+
+julia> BLRemoteDataClient.prefixes()
+3-element Vector{String}:
+ "/datax"
+ "/datax2"
+ "/datax3"
+```
+
+## Using the client with multiple servers
+
+Julia's broadcasting mechanism provides a convenient way to query multiple hosts
+with one call:
+
+```julia
+hosts = ["blc$i$j" for i in 0:7 for j in 0:7] # "blc00" to "blc77" 
+all_session_dirs = finddirs.("/datax/dibas", hosts; regex="AGBT..._999")
+```
+
+When querying many servers it may be beneficial to parallelize the queries by
+broadcasting `fetch` over a `Threads.@spawn` generator:
+
+```julia
+all_session_dirs = fetch.(Threads.@spawn BLRemoteDataClient.finddirs("/datax/dibas", h, regex="AGBT..._999") for h in hosts)
+```
---
+++ src/BLRemoteDataClient.jl
@@ -0,0 +1,147 @@
+module BLRemoteDataClient
+
+using HTTP, JSON
+
+"""
+A `Ref` holding the default hostname of the server.  This is initialized to the
+value of `ENV["BL_REMOTE_DATA_SERVER_HOST"]` if set, otherwise `"localhost"`,
+but users are free to change this at runtime as desired.
+"""
+const HOST = Ref{String}()
+
+"""
+A `Ref` holding the default port of the server.  This is initialized to the
+value of `ENV["BL_REMOTE_DATA_SERVER_PORT"]` if set, otherwise `8000`, but users
+are free to change this at runtime as desired.
+"""
+const PORT = Ref{Int}()
+
+function __init__()
+    HOST[] = get(ENV, "BL_REMOTE_DATA_SERVER_HOST", "127.0.0.1")
+    p = something(
+        tryparse(Int, get(ENV, "BL_REMOTE_DATA_SERVER_PORT", "8000")),
+        8000
+    )
+    PORT[] = p
+end
+
+"""
+    url(path, [host, [port]])
+
+Return the URL of the server using the given `path`, `host`, and `port`.  Note
+that `host` and `port` default to `HOST[]` and `PORT[]`, resp., if not given.
+"""
+function url(path, host=HOST[], port=PORT[])
+    "http://$(host):$(port)/$path"
+end
+
+function restcall(f::Function, path, host=HOST[], port=PORT[]; kwargs...)
+    query = [string(k)=>string(v) for (k,v) in kwargs]
+    resp = HTTP.get(url(path, host, port); query)
+    if resp.status == HTTP.StatusCodes.OK
+        f(resp)
+    else
+        error("got unexpected status code $(resp.status)")
+    end
+end
+
+function restcall(path, host=HOST[], port=PORT[]; kwargs...)
+    restcall(path, host, port; kwargs...) do resp
+        JSON.Parser.parse(String(resp.body))
+    end
+end
+
+"""
+    prefixes([host, [port]])::Vector{String}
+
+Return the list of directory path prefixes that the server serves.
+"""
+function prefixes(host=HOST[], port=PORT[])::Vector{String}
+    restcall("prefixes", host, port)
+end
+
+"""
+    readdir(dir, [host, [port]]; regex=".", join=true)::Vector{String}
+
+Return the list of directories and files that are in directory `dir`, which must
+be in/under one of the directories being served by the server.  `regex` can be
+used to limit the results to names that match the regular expression.  Note that
+the regular expression is parsed on the server so this just needs to be a String
+here.  If `join` is `true`, the default, the names will be prepended with `dir`.
+"""
+function readdir(dir, host=HOST[], port=PORT[]; regex=".", join=true)::Vector{String}
+    restcall("readdir", host, port; dir, regex, join)
+end
+
+"""
+    finddirs(dir, [host, [port]]; regex=".", join=true)::Vector{String}
+
+Returns a `Vector` of all directories in and recursively under directory `dir`,
+which must be in/under one of the directories being served by the server.
+`regex` can be used to limit the results to names that match the regular
+expression.  Note that the regular expression is parsed on the server so this
+just needs to be a String here.  If `join` is `true`, the default, the names
+will be prepended with `dir`.
+"""
+function finddirs(dir, host=HOST[], port=PORT[]; regex=".", join=true)::Vector{String}
+    restcall("finddirs", host, port; dir, regex, join)
+end
+
+"""
+    findfiles(dir, [host=, [port]]; regex=".", join=true)::Vector{String}
+
+Returns a `Vector` of all files in and recursively under directory `dir`, which
+must be in/under one of the directories being served by the server.  `regex` can
+be used to limit the results to names that match the regular expression.  Note
+that the regular expression is parsed on the server so this just needs to be a
+String here.  If `join` is `true`, the default, the names will be prepended with
+`dir`.
+"""
+function findfiles(dir, host=HOST[], port=PORT[]; regex=".", join=true)::Vector{String}
+    restcall("findfiles", host, port; dir, regex, join)
+end
+
+"""
+    fbfiles(dir, [host, [port]]; regex="\\.(fil|h5)\$")::Vector{Dict{String,Any}}
+
+Finds all files in/under directory `dir` that match `regex` and returns a
+`Vector` of dictionaries each containing the header metadata from a
+*Filterbank* file plus `hostname` and `filename` fields.  Matching files
+that fail to parse as a *Filterbank* file will have dictionaries containing only
+these two "extra" fields.
+
+This function works with both *SIGPROC Filterbank* files (typically having a
+`.fil` extension) and *Filterbank HDF5* files (typically having a `.h5`
+extension).
+"""
+function fbfiles(dir, host=HOST[], port=PORT[]; regex="\\.(fil|h5)\$")::Vector{Dict{String,Any}}
+    restcall("fbfiles", host, port; dir, regex)
+end
+
+"""
+    fbdata(fbname, [host, [port]]; chans=:, ifs=:, times=:, dropdims=())::Array{Float32}
+
+Returns an `Array{Float32}` containing data from the server's *Filterbank* file
+named `fbname`.  All or some of the data can be requested by passing the desired
+indices of the channel, IF, and time axes using the `chans`, `ifs`, and `times`
+keyword argument, resp.  Indices may be `Colon` (i.e. `:`), `Integer` (e.g.
+`1`), or an `Integer` range (e.g. `1:1024`).  Note that integer indices are
+1-based.  The returned `Array` will be 3-dimensional unless the `dropdims`
+keyword argument specifies a dimension or dimensions to drop.  `dropdims` can be
+an integer or a tuple of integer values to drop multiple dimensions.  Requesting
+to drop a dimension that is greater than 1 will result in an error.
+
+This function works with both *SIGPROC Filterbank* files (typically having a
+`.fil` extension) and *Filterbank HDF5* files (typically having a `.h5`
+extension).
+"""
+function fbdata(fbname, host=HOST[], port=PORT[]; chans=:, ifs=:, times=:, dropdims=())::Array{Float32}
+    restcall("fbdata", host, port; file=fbname, chans, ifs, times) do resp
+        ii = findfirst(p->p[1]=="X-dims", resp.headers)
+        dims = parse.(Int, split(resp.headers[ii][2] , ",")) |> Tuple
+        data = collect(reshape(reinterpret(Float32, resp.body), dims))
+        Base.dropdims(data; dims=dropdims)
+    end
+end
+
+end # module BLRemoteDataClient
